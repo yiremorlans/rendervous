7a4ea4e16d80893eb8d8d9f4eff8ecc9c5aac175
Yire Morlans <yire.m@icloud.com>
2022-09-12 18:29:49 -0400

    add sign up styles

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const validator = require("validator");
 const User = require("../models/User");
+const Post = require("../models/Post");
 
 exports.getLogin = (req, res) => {
   if (req.user) {
@@ -66,8 +67,8 @@ exports.postLogin = (req, res, next) => {
 
 exports.logout = (req, res) => {
   req.logout(() => {
-    console.log('User has logged out.')
-  })
+      console.info('User has logged out.')
+    })
   req.session.destroy((err) => {
     if (err)
       console.log("Error : Failed to destroy the session during logout.", err);
