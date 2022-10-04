e0d3f602e2296e9807281368496325ea30d37b66
Yire Morlans <yire.m@icloud.com>
2022-10-04 16:20:46 -0400

    readme update

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,4 +27,15 @@ module.exports = {
     },
   },
   plugins: [require("daisyui")],
+
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: false,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
