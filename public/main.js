e58631d74c812ebaf7d7e91d1157d422a70a899a
Yire Morlans <yire.m@icloud.com>
2022-10-16 14:00:21 -0400

    feat: add guest login

---
+++ public/main.js
@@ -0,0 +1,7 @@
+document.querySelector('#guestAccount').addEventListener('click',fillLogin);
+
+function fillLogin(){
+    document.querySelector('#exampleInputEmail1').value = 'Guest@rendervous.app'
+    document.querySelector('#exampleInputPassword1').value = 'renderguest2121'
+
+}
